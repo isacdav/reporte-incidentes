8227506ef4455c60baf6404a0be33a5133de267a
Steven Guevara <steven.guevara@grupobabel.com>
2020-04-20 19:02:33 -0600

    Se agrega funcionalidad combos provincias

--- ReporteIncidentes.App/src/app/incidencias/nueva/nueva.component.ts
+++ ReporteIncidentes.App/src/app/incidencias/nueva/nueva.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Router } from '@angular/router';
+import { IncidenciasService } from 'src/app/services/Incidencias.servic';
 
 @Component({
   selector: 'app-nueva',
@@ -12,8 +13,11 @@ export class NuevaComponent implements OnInit {
   name = 'Mapa';
   lat: any;
   lng: any;
-
-  constructor(public usuarioSrv: UsuarioService, private router: Router) {
+  provincias=[];
+  cantones=[];
+  distritos=[];
+  idProvincia=0;
+  constructor(public usuarioSrv: UsuarioService, private router: Router,private _incidenciasService:IncidenciasService) {
     if (this.usuarioSrv.estaLogueado()) {
       this.router.navigate(['/loguearse']);
     }
@@ -26,5 +30,51 @@ export class NuevaComponent implements OnInit {
     }
   }
 
-  ngOnInit() {}
+  async getProvincias(){
+    var resultados=await this._incidenciasService.provincias();
+    for(let index in resultados){
+      let provincia={
+        id:+index,
+        descripcion:resultados[index]
+      }
+      this.provincias.push(provincia);
+    }    
+  }
+
+  async getCantones(idProvincia:any){
+    this.cantones=[];
+    var resultados= await this._incidenciasService.cantones(idProvincia);
+    for(let index in resultados){
+      let canton={
+        id:+index,
+        descripcion:resultados[index]
+      }
+      this.cantones.push(canton);
+    }
+  }
+
+  async getDistritos(idProvincia:any,idCanton:any){
+    this.distritos=[];
+    var resultados=await this._incidenciasService.distritos(idProvincia,idCanton);
+    for(let index in resultados){
+      let distrito={
+        id:+index,
+        descripcion:resultados[index]
+      }
+      this.distritos.push(distrito);
+    }
+  }
+
+    onOptionsSelectedProvincia(event:any){
+      this.idProvincia=event.target.value;
+      this.getCantones(event.target.value);
+  }
+
+  onOptionsSelectedCantones(event:any){
+    this.getDistritos(this.idProvincia,event.target.value);
+  }
+
+  ngOnInit() {
+    this.getProvincias();
+  }
 }
---
+++ ReporteIncidentes.App/src/app/services/Incidencias.servic.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IncidenciasService {
+
+  constructor(private http: HttpClient) { }
+
+  public provincias(){
+    return this.http.get('https://ubicaciones.paginasweb.cr/provincias.json').toPromise();
+  }
+
+  public cantones(idProvincia:any){
+    return this.http.get('https://ubicaciones.paginasweb.cr/provincia/'+idProvincia+'/cantones.json').toPromise();
+  }
+
+  public distritos(idProvincia:any,idDistritos:any){
+    return this.http.get('https://ubicaciones.paginasweb.cr/provincia/'+idProvincia+'/canton/'+idDistritos+'/distritos.json').toPromise();
+  }
+}
